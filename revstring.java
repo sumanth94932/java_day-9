a101b29fac7a0a284f92ca34b90832ac6fee6a09
sumanth94932 <sumanthreddyr126@gmail.com>
2024-12-10 16:20:19 +0530

    Add files via upload

---
+++ revstring.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class rev
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        String a=s.nextLine();
+        char b[]=a.toCharArray();
+        int c=b.length;
+        int l=0;
+        int h=c-1;
+        while(l<=h)
+        {
+            char temp=b[l];
+            b[l]=b[h];
+            b[h]= temp;
+
+            l++;
+            h--;
+        }
+        String res=new String(b);
+        System.out.println(res);
+
+    }
+}/*
+         sumanth
+         htnamus   */                                        
