fdb75ab5daa3f7d3bbb8360d049a235d57ba4d5d
sumanth94932 <sumanthreddyr126@gmail.com>
2024-12-10 16:20:34 +0530

    Add files via upload

---
+++ split.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class split{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        String a=s.nextLine();
+        String b[]=a.split(" ");
+        System.out.println(Arrays.toString(b));
+    }
+}
+        /*       SUMANTH KUMAR REDDY  
+                 [SUMANTH, KUMAR, REDDY]   */
