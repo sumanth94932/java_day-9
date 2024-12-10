bf88499090c07dafde9b0d9b043c903a0725721a
sumanth94932 <sumanthreddyr126@gmail.com>
2024-12-10 16:17:52 +0530

    Add files via upload

---
+++ each.java
@@ -0,0 +1,11 @@
+import  java.util.*;
+class each{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        String a[]={"apple","orange","mango"};
+        for(String b:a)
+        System.out.println(b+" ");
+    }
+}/*       apple 
+          orange 
+          mango   */ 
